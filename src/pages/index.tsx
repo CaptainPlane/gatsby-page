0de35a405f54939c77921e221ed341896ce552b6
CaptainPlan <99226632+CaptainPlan@users.noreply.github.com>
2022-02-27 04:15:27 +0100

    Add files via upload

---
+++ src/pages/index.tsx
@@ -0,0 +1,84 @@
+import { graphql } from 'gatsby'
+import * as React from 'react'
+import { IGatsbyImageData } from 'gatsby-plugin-image'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Gallery from '../../../gatsby-image-gallery/src'
+
+interface ImageSharpEdge {
+  node: {
+    childImageSharp: {
+      thumb: IGatsbyImageData
+      full: IGatsbyImageData
+    }
+  }
+}
+
+interface PageProps {
+  data: {
+    images: {
+      edges: ImageSharpEdge[]
+    }
+  }
+}
+
+const IndexPage: React.FC<PageProps> = ({ data }) => {
+  const images = data.images.edges.map(({ node }, index) => ({
+    ...node.childImageSharp,
+    // Generate name based on the index as caption.
+    caption: `Image ${index}`,
+  }))
+
+  // Override some of Lightbox options to localise labels in French
+  const lightboxOptions = {
+    imageLoadErrorMessage: 'Impossible de charger cette image',
+    nextLabel: 'Image suivante',
+    prevLabel: 'Image précédente',
+    zoomInLabel: 'Zoomer',
+    zoomOutLabel: 'Dézoomer',
+    closeLabel: 'Fermer',
+  }
+
+  //Add callback to Lightbox onCloseRequest
+  const onClose = () => {
+    console.log('Lightbox was closed')
+  }
+
+  return (
+    <Layout>
+      <SEO title="Example" />
+      <h1>Gatsby image gallery demo</h1>
+      <p>A very simple page to demo the gallery component.</p>
+      <Gallery
+        images={images}
+        lightboxOptions={lightboxOptions}
+        onClose={onClose}
+      />
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query ImagesForGallery {
+    images: allFile(
+      filter: { relativeDirectory: { eq: "gallery" } }
+      sort: { fields: name }
+    ) {
+      edges {
+        node {
+          childImageSharp {
+            thumb: gatsbyImageData(
+              width: 270
+              height: 270
+              placeholder: BLURRED
+            )
+            full: gatsbyImageData(layout: FULL_WIDTH)
+          }
+        }
+      }
+    }
+  }
+`
+
+export default IndexPage
